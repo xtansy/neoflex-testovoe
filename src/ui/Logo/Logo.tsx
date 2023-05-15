6c2aa81471dddb77200b7fbe090ec0ed2501dca0
Andrew <skid.one@inbox.ru>
2023-05-15 21:17:07 +0300

    for deplooy

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -1,12 +1,11 @@
 import "./Logo.scss";
 import { useNavigate } from "react-router-dom";
-import { ROUTES } from "@utils";
 
 export const Logo = () => {
     const navigate = useNavigate();
 
     const onClickLogo = () => {
-        navigate(ROUTES.HOME);
+        navigate("/");
     }
 
     return <h1 className="logo" onClick={onClickLogo}>QPICK</h1>
